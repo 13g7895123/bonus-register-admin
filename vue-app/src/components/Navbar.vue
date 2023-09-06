742cc80a1ae1784ca553a652d4baef19207b75b9
Jarvis <13g7895123@gmail.com>
2023-09-06 20:37:47 +0800

    [edit] test icon

--- vue-app/src/components/Navbar.vue
+++ vue-app/src/components/Navbar.vue
@@ -5,7 +5,7 @@
       <!-- <el-col :span="12" class="logo-container"> -->
         <!-- <img src="../assets/logo.png" class="logo" alt="logo" /> -->
         <el-icon :size="20" color="#fff"><Setting /></el-icon>
-        <span class="title">後台管理系统</span>
+        <span class="title"> <el-icon :size="20" color="#fff"><Setting /></el-icon>後台管理系统</span>
       </el-col>
       <el-col :span="12" class="user">
         <div class="userinfo">
