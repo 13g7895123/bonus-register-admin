f6a85e0ab9cdeb1ed8c4e732db894d71c502ff01
Jarvis <13g7895123@gmail.com>
2023-09-06 21:11:34 +0800

    [edit] revise route

--- vue-app/src/router/routes.js
+++ vue-app/src/router/routes.js
@@ -9,11 +9,6 @@ const routes = [
         ]
     },
     {
-        path: "/register",
-        name: 'Register',
-        component: () => import('../views/Register.vue'),
-    },
-    {
         path: "/login",
         name: 'Login',
         component: () => import('../views/Login.vue'),
