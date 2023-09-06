a191e6dc005798e5ea71437459b66b9cdec906e1
Jarvis <13g7895123@gmail.com>
2023-09-06 21:50:42 +0800

    [edit] revise 404 page

--- vue-app/src/views/404.vue
+++ vue-app/src/views/404.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-full h-screen relative overflow-hidden">
         <img class="w-screen h-screen" src="../assets/404.gif" alt="error">
-        <button 
-            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 outline-none border-none focus:outline-none focus:border-none"
+        <div 
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 outline-none border-none focus:outline-none focus:border-none cursor-pointer"
             @click="$router.go(-1)"
-        >返回首頁</button>
+        >返回首頁</div>
     </div>
 </template>
 <script setup>
