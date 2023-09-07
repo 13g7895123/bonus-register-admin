3e89bd908005a6e6127b2fde39ee8ea46c843238
Jarvis <13g7895123@gmail.com>
2023-09-07 22:17:39 +0800

    [edit] test render

--- vue-app/src/views/PlayerUserEdit.vue
+++ vue-app/src/views/PlayerUserEdit.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <label>帳號</label>
-        <input>
-    </div>
+   <el-form ref="form" label-width="80px" label-position="left" >
+        <el-form-item label="活動區域" prop="area">
+            <el-input  placeholder="請輸入活動區域"></el-input>
+        </el-form-item>
+        <el-form-item prop="name">
+            <div slot="label">
+                <span>活動名稱</span>
+                <el-input placeholder="請輸入活動名稱"></el-input>
+            </div>
+        </el-form-item>
+        <el-button @click="">送出</el-button>
+    </el-form>
 </template>
