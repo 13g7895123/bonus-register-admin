8a2528056dd1f06a54e3d9c8db81b8b1fe3b0ee9
Jarvis <13g7895123@gmail.com>
2023-09-07 19:59:16 +0800

    [edit] revise code

--- vue-app/src/views/Server.vue
+++ vue-app/src/views/Server.vue
@@ -111,8 +111,8 @@ const page_index = ref(1),
 // 篩選
 const filterEngName = ref()
 
-const getSystemAdmin = async() => {
-    const { data: { success, data } } = await axios.post('/api/system_admin.php?action=system_admin')
+const getServer = async() => {
+    const { data: { success, data } } = await axios.post('/api/server.php?action=server')
 
     if (success){
         tableData.value = data
@@ -124,7 +124,7 @@ const getSystemAdmin = async() => {
     }
 }
 
-watchEffect(() => getSystemAdmin())
+watchEffect(() => getServer())
 
 const handleAdd = () => {
     show.value = true
