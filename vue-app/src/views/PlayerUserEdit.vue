a009e09717a086b953b3f6039ded02c6da6e725c
Jarvis <13g7895123@gmail.com>
2023-09-09 18:27:20 +0800

    [edit] revise input type

--- vue-app/src/views/PlayerUserEdit.vue
+++ vue-app/src/views/PlayerUserEdit.vue
@@ -15,7 +15,7 @@
                 <el-input v-model="formData.account" ></el-input>
             </el-form-item>
             <el-form-item prop="password" label="密碼">
-                <el-input v-model="formData.password" type="password" :placeholder="!props.operation ? '不修改則留空' : ''"></el-input>
+                <el-input v-model="formData.password" :placeholder="!props.operation ? '不修改則留空' : ''"></el-input>
             </el-form-item>
             <el-form-item prop="phone" label="手機">
                 <el-input v-model="formData.phone"></el-input>
