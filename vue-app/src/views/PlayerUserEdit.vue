db43218e49eadad089d5464f204b44b41ed33ac6
Jarvis <13g7895123@gmail.com>
2023-09-09 17:29:00 +0800

    [edit] test post value

--- vue-app/src/views/PlayerUserEdit.vue
+++ vue-app/src/views/PlayerUserEdit.vue
@@ -86,6 +86,7 @@ const handleSubmit = (formEl) => {
             console.log(formData.value);
             console.log(typeof(formData.value));            
             console.log(typeof(formData.value['account']));
+            console.log(typeof(formData.value.account));
 
             const ajax_data = { id: id.valur }
             const { data: { success, msg } } = await axios.post(`/api/player_user.php?action=edit_player_user`, ajax_data)
