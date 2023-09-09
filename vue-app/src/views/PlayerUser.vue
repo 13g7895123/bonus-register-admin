0e02f663de8fa6c2c89174f8a46fd5cfaa8607ff
Jarvis <13g7895123@gmail.com>
2023-09-10 00:35:04 +0800

    [edit] revise render

--- vue-app/src/views/PlayerUser.vue
+++ vue-app/src/views/PlayerUser.vue
@@ -6,7 +6,7 @@
             </div>
         </template>
     <div class="h-full px-5 py-3">
-        <div class="w-full flex items-center justify-end">
+        <div class="w-full flex items-center justify-between">
             <el-form :inline="true">
                 <el-form-item prop="colName" label="">
                     <el-input v-model="filterEngName"></el-input>
