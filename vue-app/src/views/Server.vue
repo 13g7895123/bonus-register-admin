83d72e56e16d660219d15a9fac1453794e07d841
Jarvis <13g7895123@gmail.com>
2023-09-11 00:18:49 +0800

    [edit] revise render

--- vue-app/src/views/Server.vue
+++ vue-app/src/views/Server.vue
@@ -52,7 +52,7 @@
                             >
                                 <!-- <i class="el-icon-question"> </i> -->
                                 <!-- <el-icon><QuestionFilled /></el-icon> -->
-                                <el-icon :size="5" color="#555"><QuestionFilled /></el-icon>
+                                <el-icon :size="15" color="#555"><QuestionFilled /></el-icon>
                                 <div style="width: 200px" slot="content">
                                     123
                                 </div>
