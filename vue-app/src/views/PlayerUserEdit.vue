02d17ee8896ed81b70507226819814bd8f6e9c36
Jarvis <13g7895123@gmail.com>
2023-09-10 11:50:47 +0800

    [edit] test render

--- vue-app/src/views/PlayerUserEdit.vue
+++ vue-app/src/views/PlayerUserEdit.vue
@@ -9,7 +9,7 @@
             :model="formData"
             ref="dialogRef"
             label-width="120px"
-            style="margin: 10px; width: 120px"
+            style="margin: 10px; width: 30%"
             >
             <el-form-item prop="account" label="帳號">
                 <el-input v-model="formData.account" class="text-center"></el-input>
