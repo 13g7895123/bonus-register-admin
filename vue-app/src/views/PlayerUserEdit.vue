8e151cb9440e1ddf6ed265e7dbc6d04761ac746c
Jarvis <13g7895123@gmail.com>
2023-09-10 12:45:38 +0800

    [edit] revise column width

--- vue-app/src/views/PlayerUserEdit.vue
+++ vue-app/src/views/PlayerUserEdit.vue
@@ -127,7 +127,7 @@ const handleCancel = () => {
 }
 
 .date_column .el-input,
-.date_column .el-inpu__wrapper,
+.date_column .el-input__wrapper,
 .date_column .el-form-item__content{
     width: 100%;
 }
