7ab37b2a3bc0d21ac301736e17f0b55bd576b3c3
Jarvis <13g7895123@gmail.com>
2023-09-12 00:34:58 +0800

    [edit] revise render

--- vue-app/src/router/routes.js
+++ vue-app/src/router/routes.js
@@ -5,11 +5,16 @@ const routes = [
         component: () => import('../views/Home.vue'),
         children: [
             { path: "/player", name: 'player', component: () => import('../views/PlayerUser.vue')},
-            { path: "/player/edit/:id", name: 'editPlayer', component: () => import('../views/PlayerUserEdit.vue')},
             { path: "/player/add", name: 'addPlayer', component: () => import('../views/PlayerUserAdd.vue')},
+            { path: "/player/edit/:id", name: 'editPlayer', component: () => import('../views/PlayerUserEdit.vue')},
             { path: "/server", component: () => import('../views/Server.vue')},
+            { path: "/server/add", name: 'addServer', component: () => import('../views/ServerAdd.vue')},
+            { path: "/server/edit/:id", name: 'editServer', component: () => import('../views/ServerEdit.vue')},
             { path: "/systemUser", component: () => import('../views/SystemUser.vue')},
+            { path: "/systemUser/add", name: 'addSystemUser', component: () => import('../views/SystemUserAdd.vue')},
+            { path: "/systemUser/edit/:id", name: 'editSystemUser', component: () => import('../views/SystemUserEdit.vue')},
             { path: "/systemAdmin", component: () => import('../views/SystemAdmin.vue')},
+            { path: "/systemAdmin/add", name: 'addSystemAdmin', component: () => import('../views/SystemAdminAdd.vue')},
             { path: "/systemAdmin/edit/:id", name: 'editSystemAdmin', component: () => import('../views/SystemAdminEdit.vue')},
         ]
     },
