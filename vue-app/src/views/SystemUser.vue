49f28e4251cfe1cc804d8ac3c3c1739a2ab9d67f
Jarvis <13g7895123@gmail.com>
2023-09-11 23:17:48 +0800

    [edit] revise render

--- vue-app/src/views/SystemUser.vue
+++ vue-app/src/views/SystemUser.vue
@@ -2,7 +2,7 @@
     <el-card class="box-card mx-auto mt-3">
         <template #header>
             <div class="card-header text-left">
-                <span class="text-xl font-semibold">{{ $route.path }}</span>
+                <span class="text-xl font-semibold">{{ $route.name }}</span>
             </div>
         </template>
         <div class="h-full px-5 py-3">
