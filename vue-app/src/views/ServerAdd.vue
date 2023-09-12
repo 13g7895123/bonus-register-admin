4ab0d40ce028a12da96c3abd21769278794c735e
Jarvis <13g7895123@gmail.com>
2023-09-12 22:37:45 +0800

    [edit] revise function

--- vue-app/src/views/ServerAdd.vue
+++ vue-app/src/views/ServerAdd.vue
@@ -9,7 +9,8 @@
             <el-form
                 :model="formData"
                 ref="dialogRef"
-                style="margin: 10px; width: 100;"
+                label-width="100px"
+                style="margin: 10px;"
                 >
                 <el-form-item prop="name" label="伺服器名稱">
                     <el-input v-model="formData.name" class="text-center"></el-input>
