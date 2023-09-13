5809fa2f1130974c4519337eaf317bcb35cb136a
Jarvis <13g7895123@gmail.com>
2023-09-13 21:14:52 +0800

    [edit] test

--- vue-app/src/views/SystemUserAdd.vue
+++ vue-app/src/views/SystemUserAdd.vue
@@ -59,7 +59,7 @@ const apiUrlPrefix = ref('/api/')
 const dialogRef = ref()
 const nowUser = ref()
 const isAdmin = ref()
-let serverList = {}
+let serverList = []
 const checkboxGroup1 = ref(['test_server'])
 
 // Api config
@@ -129,8 +129,8 @@ const getServer = async() => {    // 依操作者權限取得伺服器列表
     if (success){
         const serverListData = data
         for (let i = 0; i < data.length; i++){
-            serverList[i]['name'] = serverListData[i]['name']
-            serverList[i]['code_name'] = serverListData[i]['code_name']
+            serverList[i].name = serverListData[i]['name']
+            serverList[i].code_name = serverListData[i]['code_name']
         }
     }
 }
