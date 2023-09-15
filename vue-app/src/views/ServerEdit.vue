676d8a1aab20bf1bdb8b067ac11990fa6255de2b
Jarvis <13g7895123@gmail.com>
2023-09-15 21:56:41 +0800

    [edit] test file upload

--- vue-app/src/views/ServerEdit.vue
+++ vue-app/src/views/ServerEdit.vue
@@ -46,7 +46,7 @@
                     <el-upload
                     ref="uploadRef"
                     class="upload-demo"
-                    action="{{ imgUploadUrl.value }}"
+                    action="http://170.187.229.132:9091/api/bonus-register/admin/server.php?action=bg_img_upload"
                     :multiple="false"
                     :show-file-list="true"
                     :file-list="fileList"
