e44cabc0b9ae4239077c5aca8fd06901f4e5143e
Jarvis <13g7895123@gmail.com>
2023-09-16 20:11:34 +0800

    [edit] test

--- vue-app/src/views/ServerEdit.vue
+++ vue-app/src/views/ServerEdit.vue
@@ -111,7 +111,7 @@ onMounted(() => {
     // imgUploadUrl.value = url
 
     // imgUploadUrl.value = '/testApi/api-ori/api/bonus-register/admin/server.php?action=bg_img_upload'
-    imgUploadUrl.value = 'http://missa.mercylife.cc/img_upload/upload.php'
+    imgUploadUrl.value = 'http://missa.mercylife.cc/img_upload/upload.php?action=server'
     // imgUploadUrl.value = 'http://missa.mercylife.cc/Pages/ajax/com_company_group.php?action=bg_img_upload'
     // imgUploadUrl.value = '/api-ori/api/bonus-register/admin/testApi/server.php?action=bg_img_upload'
     // console.log(imgUploadUrl.value);
