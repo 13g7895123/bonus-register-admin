daf09239e34a7185a892e5759e0a0504b311a5cc
Jarvis <13g7895123@gmail.com>
2023-09-17 00:46:40 +0800

    [edit] test

--- vue-app/src/views/Server.vue
+++ vue-app/src/views/Server.vue
@@ -34,7 +34,7 @@
                 <el-table-column label="伺服器名稱" align="center" width="auto" prop="name"/>
                 <el-table-column label="伺服器代號" align="center" width="auto" prop="code_name"/>
                 <el-table-column label="背景圖片" align="center" width="auto" prop="">
-                    <template slot-scope="scope">
+                    <template #default="scope">
                         <el-image :preview-src-list="'http://missa.mercylife.cc' + scope.row.bg_img_path"></el-image>
                     </template>
                     <!-- <el-image v-if="imgUrl != ''" style="max-width: 130px; height: auto" :src="imgUrl"/> -->
