de39982d23e91d6cde9cf4abb07b184e7bbfaa68
Jarvis <13g7895123@gmail.com>
2023-10-10 07:20:00 +0800

    test

--- vue-app/src/views/ServerBgImg.vue
+++ vue-app/src/views/ServerBgImg.vue
@@ -42,7 +42,8 @@ onMounted(() => {
     getData()
     
     // 這邊的檔案上傳不可用vite proxy，會失敗
-    imgUploadUrl.value = `http://missa.mercylife.cc/img_upload/upload.php?action=server&sid=${id.value}`
+    // imgUploadUrl.value = `http://missa.mercylife.cc/img_upload/upload.php?action=server&sid=${id.value}`
+    imgUploadUrl.value = `http://http://170.187.229.132:9091/api/bonus-register/admin/imgUpload.php?action=imgUpload&sid=${id.value}`
 })
 
 const getData = async() => {
