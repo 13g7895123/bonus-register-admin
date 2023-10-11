4258633ed51212b24deb62c9863b72292c0a19b9
Jarvis <13g7895123@gmail.com>
2023-10-11 18:26:20 +0800

    test

--- vue-app/src/utils/autoLogout.js
+++ vue-app/src/utils/autoLogout.js
@@ -53,5 +53,5 @@ const checkTimeout = () => {
 export default function () {
     /* 定时器 间隔30秒检测是否长时间未操作页面 */
     // window.setInterval(checkTimeout, 30000);
-    window.setInterval(checkTimeout, 5000);
+    // window.setInterval(checkTimeout, 5000);
 }
