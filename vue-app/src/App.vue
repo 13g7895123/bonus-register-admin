2af95e566264a5d37011ab419c79b47aaae83993
Jarvis <13g7895123@gmail.com>
2023-10-18 23:59:01 +0800

    test

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -8,7 +8,7 @@ import zhTw from 'element-plus/lib/locale/lang/zh-tw'
 const router = useRouter()
 
 onMounted(() => {
-  console.log(router.currentRoute);
+  console.log(router.currentRoute._value.path);
   if(sessionStorage.getItem('isLogin') == null){
     Swal.fire({
         title: '驗證失敗',
